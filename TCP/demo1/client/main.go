fb21b40f63ea13be4a0a80f8f4606b3d7771f414
lijianhuahtml <481802114@qq.com>
2024-04-06 02:39:09 +0800

    modify

--- TCP/demo1/client/main.go
+++ TCP/demo1/client/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:8000")
+	conn, err := net.Dial("tcp", "8.138.115.214:8000")
 	if err != nil {
 		fmt.Println("err:", err)
 		return
